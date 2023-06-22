5947d2ddae49bd4292f5884e33479dc936effd33
Arpad <arpad@Arpads-MacBook-Pro.local>
2023-06-22 00:09:02 -0400

    server running and products displayed crudely

--- app/scripts/components/menu.js
+++ app/scripts/components/menu.js
@@ -1,85 +1,138 @@
 /**
  * This file will hold the Menu that lives at the top of the Page, this is all rendered using a React Component...
- * 
+ *
  */
-import React from 'react';
+import React from "react";
 
 class Menu extends React.Component {
+  /**
+   * Main constructor for the Menu Class
+   * @memberof Menu
+   */
+  constructor() {
+    super();
+    this.state = {
+      showingSearch: false,
+      products: [],
+    };
+  }
 
-    /**
-     * Main constructor for the Menu Class
-     * @memberof Menu
-     */
-    constructor() {
-        super();
-        this.state = {
-            showingSearch: false
-        };
+  /**
+   * Shows or hides the search container
+   * @memberof Menu
+   * @param e [Object] - the event from a click handler
+   */
+  showSearchContainer(e) {
+    e.preventDefault();
+    this.setState({
+      showingSearch: !this.state.showingSearch,
+    });
+  }
+
+  updateProducts(products) {
+    this.setState({
+      products: products,
+    });
+  }
+
+  /**
+   * Calls upon search change
+   * @memberof Menu
+   * @param e [Object] - the event from a text change handler
+   */
+  async onSearch(e) {
+    console.log("e.target.value", e.target.value);
+    // Start Here
+    const name = e.target.value;
+    try {
+      const response = await fetch("http://localhost:3035/products", {
+        method: "POST",
+        // credentials: "same-origin",
+        // mode: "cors",
+        headers: {
+          //   //   "Content-Type": "X-www-form-urlencoded",
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+          //   "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ name }),
+        // body: { name },
+      });
+      const products = await response.json();
+      console.log("products", products);
+      this.updateProducts(products);
+    } catch (error) {
+      console.log("There was an error", error);
     }
+  }
 
-    /**
-     * Shows or hides the search container
-     * @memberof Menu
-     * @param e [Object] - the event from a click handler
-     */
-    showSearchContainer(e) {
-        e.preventDefault();
-        this.setState({
-            showingSearch: !this.state.showingSearch
-        });
-    }
+  /**
+   * Renders the default app in the window, we have assigned this to an element called root.
+   *
+   * @returns JSX
+   * @memberof App
+   */
+  render() {
+    return (
+      <header className="menu">
+        <div className="menu-container">
+          <div className="menu-holder">
+            <h1>ELC</h1>
+            <nav>
+              <a href="#" className="nav-item">
+                HOLIDAY
+              </a>
+              <a href="#" className="nav-item">
+                WHAT'S NEW
+              </a>
+              <a href="#" className="nav-item">
+                PRODUCTS
+              </a>
+              <a href="#" className="nav-item">
+                BESTSELLERS
+              </a>
+              <a href="#" className="nav-item">
+                GOODBYES
+              </a>
+              <a href="#" className="nav-item">
+                STORES
+              </a>
+              <a href="#" className="nav-item">
+                INSPIRATION
+              </a>
 
-    /**
-     * Calls upon search change
-     * @memberof Menu
-     * @param e [Object] - the event from a text change handler
-     */
-    onSearch(e) {
-        
-        // Start Here
-        // ...
-        
-
-    }
-
-    /**
-     * Renders the default app in the window, we have assigned this to an element called root.
-     * 
-     * @returns JSX
-     * @memberof App
-    */
-    render() {
-        return (
-            <header className="menu">
-                <div className="menu-container">
-                    <div className="menu-holder">
-                        <h1>ELC</h1>
-                        <nav>
-                            <a href="#" className="nav-item">HOLIDAY</a>
-                            <a href="#" className="nav-item">WHAT'S NEW</a>
-                            <a href="#" className="nav-item">PRODUCTS</a>
-                            <a href="#" className="nav-item">BESTSELLERS</a>
-                            <a href="#" className="nav-item">GOODBYES</a>
-                            <a href="#" className="nav-item">STORES</a>
-                            <a href="#" className="nav-item">INSPIRATION</a>
-
-                            <a href="#" onClick={(e) => this.showSearchContainer(e)}>
-                                <i className="material-icons search">search</i>
-                            </a>
-                        </nav>
-                    </div>
+              <a href="#" onClick={(e) => this.showSearchContainer(e)}>
+                <i className="material-icons search">search</i>
+              </a>
+            </nav>
+          </div>
+        </div>
+        <div
+          className={
+            (this.state.showingSearch ? "showing " : "") + "search-container"
+          }
+        >
+          <input type="text" onChange={(e) => this.onSearch(e)} />
+          <a href="#" onClick={(e) => this.showSearchContainer(e)}>
+            <i className="material-icons close">close</i>
+          </a>
+          <div className="search-results">
+            {this.state.products.map((product) => {
+              return (
+                <div className="search-result">
+                  <img src={product.picture} />
+                  <div className="search-result-info">
+                    <h3>{product.name}</h3>
+                    <p>{product.price}</p>
+                  </div>
                 </div>
-                <div className={(this.state.showingSearch ? "showing " : "") + "search-container"}>
-                    <input type="text" onChange={(e) => this.onSearch(e)} />
-                    <a href="#" onClick={(e) => this.showSearchContainer(e)}>
-                        <i className="material-icons close">close</i>
-                    </a>
-                </div>
-            </header>
-        );
-    }
-
-
+              );
+            })}
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 // Export out the React Component
--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 /**
  * The Server Can be configured and created here...
- * 
+ *
  * You can find the JSON Data file here in the Data module. Feel free to impliment a framework if needed.
  */
 
@@ -19,26 +19,85 @@
     ]
 }
 */
-const data      = require('./data');
-const http      = require('http');
-const hostname  = 'localhost';
-const port      = 3035;
+const data = require("./data");
+const http = require("http");
+const HOST = "localhost";
+const PORT = 3035;
 
-/** 
+/**
  * Start the Node Server Here...
- * 
- * The http.createServer() method creates a new server that listens at the specified port.  
- * The requestListener function (function (req, res)) is executed each time the server gets a request. 
+ *
+ * The http.createServer() method creates a new server that listens at the specified port.
+ * The requestListener function (function (req, res)) is executed each time the server gets a request.
  * The Request object 'req' represents the request to the server.
  * The ServerResponse object 'res' represents the writable stream back to the client.
  */
-http.createServer(function (req, res) {
+
+function getProducts(name = "", res) {
+  console.log("name", name);
+  const responseData = data.filter((item) =>
+    item.name.toLowerCase().startsWith(name.toLowerCase())
+  );
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+  });
+  res.write(JSON.stringify(responseData));
+  //   res.write(JSON.stringify(data));
+  res.end();
+}
+
+function controller(req, res) {
+  //   console.log("req", req);
+  let body = [];
+  req
+    .on("data", (chunk) => {
+      console.log("chunk", chunk);
+      body = JSON.parse(chunk.toString());
+      console.log("body", body);
+    })
+    .on("end", () => {
+      console.log("req.url", req.url);
+      console.log("body", body);
+      switch (req.url) {
+        case "/products":
+          return getProducts(body.name, res);
+        default:
+          return res.end();
+      }
+    });
+}
+
+http
+  .createServer(function (req, res) {
     // .. Here you can create your data response in a JSON format
-    
-    
-    res.write("Response goes in here..."); // Write out the default response
-    res.end(); //end the response
-}).listen( port );
+    // console.log("req", req);
+    // Object.keys(req).forEach((key) => {
+    //   console.log("key", key);
+    // });
+    // let body = [];
+    // req.on("data", (chunk) => {
+    //   console.log("chunk", chunk);
+    //   body = chunk;
+    //   console.log("body", JSON.parse(body.toString()));
+    // });
+    // console.log("req.url", req.url);
+    // console.log("body", body);
+    // console.log("req.body", req.body);
+    // res.writeHead(200, {
+    //   "Content-Type": "application/json",
+    //   "Access-Control-Allow-Origin": "*",
+    //   "Access-Control-Allow-Headers":
+    //     "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+    // });
+    // //   res.write(JSON.stringify(responseData));
+    // res.write(JSON.stringify(data));
+    // res.end();
+    controller(req, res);
+    // res.write("Response goes in here..."); // Write out the default response
+    // res.end(); //end the response
+  })
+  .listen(PORT);
 
-
-console.log(`[Server running on ${hostname}:${port}]`);
+console.log(`[Server running on ${HOST}:${PORT}]`);
