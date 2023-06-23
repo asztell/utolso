f2403047548ff7f391155531fdbafb9c3b65c71f
Arpad <arpad@Arpads-MacBook-Pro.local>
2023-06-22 23:39:53 -0400

    remove unnecessary wrapper

--- app/scripts/components/products.jsx
+++ app/scripts/components/products.jsx
@@ -13,20 +13,18 @@ export default function Products() {
   }
 
   return (
-    <>
-      <ul onMouseLeave={onMouseLeave} className="search-results">
-        {products.slice(0, 4).map((product) => {
-          return (
-            <li key={product._id} className="product-card">
-              <img src={product.picture} />
-              <div className="product-info">
-                <h3>{product.name.toUpperCase()}</h3>
-                <p>{product.about}</p>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <ul onMouseLeave={onMouseLeave} className="search-results">
+      {products.slice(0, 4).map((product) => {
+        return (
+          <li key={product._id} className="product-card">
+            <img src={product.picture} />
+            <div className="product-info">
+              <h3>{product.name.toUpperCase()}</h3>
+              <p>{product.about}</p>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
